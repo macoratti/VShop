d26d4661f26e1f6b5f51fe79fa30129511e06b48
Macoratti <macoratti@yahoo.com>
2022-05-16 07:38:55 -0300

    criação dos serviços e controladores

---
+++ VShop.ProductApi/Controllers/ProductsController.cs
@@ -0,0 +1,82 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using VShop.ProductApi.DTOs;
+using VShop.ProductApi.Services;
+
+namespace VShop.ProductApi.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class ProductsController : ControllerBase
+    {
+        private readonly IProductService _productService;
+        public ProductsController(IProductService productService)
+        {
+            _productService = productService;
+        }
+        [HttpGet]
+        public async Task<ActionResult<IEnumerable<ProductDTO>>> Get()
+        {
+            var produtosDto = await _productService.GetProducts();
+            if (produtosDto == null)
+            {
+                return NotFound("Products not found");
+            }
+            return Ok(produtosDto);
+        }
+
+        [HttpGet("{id}", Name = "GetProduct")]
+        public async Task<ActionResult<ProductDTO>> Get(int id)
+        {
+            var produtoDto = await _productService.GetProductById(id);
+            if (produtoDto == null)
+            {
+                return NotFound("Product not found");
+            }
+            return Ok(produtoDto);
+        }
+
+        [HttpPost]
+        public async Task<ActionResult> Post([FromBody] ProductDTO produtoDto)
+        {
+            if (produtoDto == null)
+                return BadRequest("Data Invalid");
+
+            await _productService.AddProduct(produtoDto);
+
+            return new CreatedAtRouteResult("GetProduct",
+                new { id = produtoDto.Id }, produtoDto);
+        }
+
+        [HttpPut("{id}")]
+        public async Task<ActionResult> Put(int id, [FromBody] ProductDTO produtoDto)
+        {
+            if (id != produtoDto.Id)
+            {
+                return BadRequest("Data invalid");
+            }
+
+            if (produtoDto == null)
+                return BadRequest("Data invalid");
+
+            await _productService.UpdateProduct(produtoDto);
+
+            return Ok(produtoDto);
+        }
+
+        [HttpDelete("{id}")]
+        public async Task<ActionResult<ProductDTO>> Delete(int id)
+        {
+            var produtoDto = await _productService.GetProductById(id);
+
+            if (produtoDto == null)
+            {
+                return NotFound("Product not found");
+            }
+
+            await _productService.RemoveProduct(id);
+
+            return Ok(produtoDto);
+        }
+    }
+}
--- VShop.ProductApi/Program.cs
+++ VShop.ProductApi/Program.cs
@@ -1,5 +1,7 @@
 using Microsoft.EntityFrameworkCore;
 using VShop.ProductApi.Context;
+using VShop.ProductApi.Repositories;
+using VShop.ProductApi.Services;
 
 var builder = WebApplication.CreateBuilder(args);
 
@@ -17,6 +19,12 @@ builder.Services.AddDbContext<AppDbContext>(options =>
                     ServerVersion.AutoDetect(mySqlConnection)));
 
 builder.Services.AddAutoMapper(AppDomain.CurrentDomain.GetAssemblies());
+
+builder.Services.AddScoped<ICategoryRepository, CategoryRepository>();
+builder.Services.AddScoped<IProductRepository, ProductRepository>();
+builder.Services.AddScoped<ICategoryService, CategoryService>();
+builder.Services.AddScoped<IProductService, ProductService>();
+
 
 var app = builder.Build();
 
